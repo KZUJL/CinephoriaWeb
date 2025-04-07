4ab36d872510d72deb7030c25a1f790bec441056
Jean-Loup <jean-loup.quazuguel@zf.com>
2025-04-07 13:38:59 +0200

    Featur3/footer

--- CinephoriaWeb/src/models/types.ts
+++ CinephoriaWeb/src/models/types.ts
@@ -1,11 +1,26 @@
-// Définition des types pour les films
+// Définition des types 
 export interface Film {
     movieId: number;
     title: string;
-    poster: string; // L'URL de l'affiche
+    poster: string; 
   }
+
+export interface Cinema {
+    cinemaId: number;
+    name: string;
+    address: string;
+    country: string;
+    city: string;
+    phoneNumber: string;
+    schedules: Schedule[];
+}
+
+export interface Schedule {
+    scheduleId: number;
+    cinemaId: number;
+    jour: string;
+    heure_ouverture: string;
+    heure_fermeture: string;
+}
   
-  export interface MoviesResponse {
-    nouveautes: Film[];
-  }
-  
+ 
--- CinephoriaWeb/src/services/apiCinephoria.ts
+++ CinephoriaWeb/src/services/apiCinephoria.ts
@@ -3,6 +3,11 @@ import axios from 'axios';
 export default class ApiCinephoria {
     private API_URL: string;
 
+    /**
+     * Constructeur de la classe ApiCinephoria.
+     * Initialise l'URL de base de l'API à partir des variables d'environnement.
+     * @throws {Error} Lance une erreur si l'URL de l'API n'est pas définie.
+     */
     constructor() {
         this.API_URL = import.meta.env.VITE_API_URL;
         if (!this.API_URL) {
@@ -11,7 +16,14 @@ export default class ApiCinephoria {
         }
     }
 
-    // Méthode générique pour gérer les requêtes
+    /**
+     * Méthode générique pour effectuer une requête GET vers un endpoint spécifique.
+     * @private
+     * @param {string} endpoint - Le chemin de l'endpoint à appeler (ex. '/api/Movies').
+     * @returns {Promise<any>} Une promesse résolue avec les données de la réponse.
+     * @throws {Error} Lance une erreur si la requête échoue.
+     */
+
     private fetchMovies(endpoint: string) {
         console.log(`Fetching movies from ${this.API_URL}${endpoint}`);
         return axios
@@ -26,15 +38,44 @@ export default class ApiCinephoria {
             });
     }
 
+    /**
+     * Récupère tous les films disponibles via l'API.
+     * @returns {Promise<any>} Une promesse résolue avec les données des films.
+     */
     getMovies() {
         return this.fetchMovies('/api/Movies');
     }
-
+    /**
+     * Récupère les films actuellement disponibles via l'API.
+     * @returns {Promise<any>} Une promesse résolue avec les données des films disponibles.
+     */
     getAvailableMovies() {
         return this.fetchMovies('/api/Movies/available');
     }
-
+    /**
+     * Récupère les films qui seront bientôt disponibles via l'API.
+     * @returns {Promise<any>} Une promesse résolue avec les données des films à venir.
+     */
     getSoonAvailableMovies() {
         return this.fetchMovies('/api/Movies/soon-available');
+    }
+
+
+
+    private fetchCinemas(endpoint: string) {
+        console.log(`Fetching cinemas from ${this.API_URL}${endpoint}`);
+        return axios
+            .get(`${this.API_URL}${endpoint}`)
+            .then((response) => {
+                console.log(`${endpoint} API response:`, response.data);
+                return response.data; 
+            })
+            .catch((error) => {
+                console.error(`Error fetching cinemas from ${endpoint}:`, error);
+                throw error;  
+            });
+    }
+    getCinemas() {
+        return this.fetchCinemas('/api/Cinema');
     }
 }
